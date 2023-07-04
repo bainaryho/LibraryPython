4a38aa2755543671247baa728b547297da419b6c
bainaryho <rkswl233@naver.com>
2023-07-05 00:29:38 +0900

    DELETE book functions ADD show.py UPDATE main.py

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
-# 도서관리 콘솔 어플리케이션 프로젝트
+# 도서 관리 콘솔 어플리케이션
+from book import Book
+from show import *
+
+book1 = Book('1', '제목', '이진호', '사자출판사', '대출가능')
 
 while 1:
+    show_main()
+    main_choice = int(input('|-> 사용자 선택 : '))
     print('+-------------------------')
-    print('|도서관 관리 시스템 메인 메뉴')
-    print('|-------------------------')
-    print('| 1. 도서 정보 조회')
-    print('| 2. 도서 대출')
-    print('| 3. 도서 반납')
-    print('| 4. 대출 정보 조회')
-    print('| 5. 종료')
-    print('|-------------------------')
-    main_choice = int(input('+ -> 사용자 선택 : '))
 
-    if main_choice == 1:
-        pass
-    elif main_choice == 2:
-        pass
-    elif main_choice == 3:
-        pass
-    elif main_choice == 4:
-        pass
-    elif main_choice == 5:
+    if main_choice == 1:  # 도서 정보 조회
+        book1.book_info()
+
+    elif main_choice == 2:  # 도서 대출
+        show_sub('대출')
+        book1.book_loan()
+
+    elif main_choice == 3:  # 도서 반납
+        show_sub('반납')
+        book1.book_return()
+
+    elif main_choice == 4:  # 대출 정보 조회
+        book1.book_loan_info()
+
+    elif main_choice == 5:  # 도서 추가
+        book1.book_input()
+
+    elif main_choice == 0:  # 종료
         break
+
     else:
-        print('다시 입력 해주세요')
+        print('다시 입력')
