62ba17ab1911087dbe656d186269ee6a2f26a390
bainaryho <rkswl233@naver.com>
2023-07-05 18:48:18 +0900

    ADD show.py 화면구성 기능 추가

--- show.py
+++ show.py
@@ -2,13 +2,17 @@ def show_main():
     print('+-------------------------')
     print('|도서관 관리 시스템 메인 메뉴')
     print('|-------------------------')
-    print('| 1. 도서 정보 조회')
+    print('| 1. 도서 찾기')
     print('| 2. 도서 대출')
     print('| 3. 도서 반납')
-    print('| 4. 대출 정보 조회')
+    print('| 4. 대출 조회')
     print('| 5. 도서 추가')
+    print('| 6. 전체 도서 조회')
     print('| 0. 종료')
     print('|-------------------------')
+    main_choice = int(input('|-> 사용자 선택 : '))
+    print('+-------------------------')
+    return main_choice
 
 
 def show_sub(service):
@@ -20,3 +24,16 @@ def show_sub(service):
     choice = input('|-> 사용자 입력 : ')
     print('+-------------------------\n')
     return choice
+
+
+def show_add():
+    print('+-------------------------')
+    print('|도서 입력 메뉴')
+    print('|-------------------------')
+    bid = input('|도서ID를 입력하세요 :')
+    title = input('|도서명을 입력하세요 :')
+    author = input('|저자를 입력하세요 :')
+    publisher = input('|출판사를 입력하세요 :')
+    print('+-------------------------')
+    book = [bid, title, author, publisher]
+    return book
