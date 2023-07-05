ab386a6df3f13ff25bc8f438482f44ef99b95e43
bainaryho <rkswl233@naver.com>
2023-07-05 18:49:28 +0900

    Update book.py 정보조회, 대출, 전체조회, 추가 기능

--- book.py
+++ book.py
@@ -1,44 +1,105 @@
 # 도서에 대한 클래스 선언
+from database import database
+
+
 class Book:
-    def __init__(self, id, title, author, publisher, is_available):
+    def __init__(self, id, title, author, publisher):
         self.id = id
         self.title = title
         self.author = author
         self.publisher = publisher
-        self.is_available = is_available
 
-    def book_info(self):
-        # (기본) 사용자는 도서의 ID 혹은 이름을 입력하여 도서의 정보를 조회 합니다.
-        # (기본) 도서의 정보는 도서의 ID, 이름, 저자, 출판사 정보를 포함합니다.
-        # (심화) 도서의 상태(대출 가능, 대출 중)가 표시됩니다.
-        # (심화) 도서의 상태는 도서가 대출 가능한 상태인지, 대출 중인 상태인지를 나타냅니다.
-        # (심화) 도서가 대출 중인 상태인 경우, 도서의 대출 정보를 함께 출력합니다.
-        pass
+    def book_input(self, db):
+        sql = "INSERT INTO Books VALUES (%s,%s,%s,%s, TRUE);"
+        db.cursor.execute(sql, (int(self.id), self.title, self.author, self.publisher))
+        db.conn.commit()
+        return
 
-    def book_loan(self):
-        # 도서 대출 기능 (기본)
-        # (기본) 사용자는 콘솔을 통해 도서의 ID 혹은 이름을 입력하여 도서를 대출합니다.
-        # (기본) 대출하면 도서의 상태를 대출중으로 변경합니다.
-        # (심화) 대출중인 도서를 모두 출력합니다.
-        # (심화) 도서가 이미 대출 중일 경우, 대출이 불가능하다고 출력합니다.
-        pass
 
-    def book_return(self):
-        # 도서 반납 기능 (기본)
-        # (기본) 반납을 원하는 도서의 ID 혹은 이름을 입력하여 반납합니다.
-        # (기본) 반납하면 도서의 상태가 대출 가능으로 변경됩니다.
-        pass
+def book_info(searcher, db):
+    if searcher.isdigit():
+        sql = "SELECT * FROM Books WHERE book_id= %s "
+        db.cursor.execute(sql, searcher)
+    else:
+        like_word = '\'%'+searcher+'%\''
+        sql = "SELECT * FROM Books WHERE title LIKE %s" % (like_word)
+        db.cursor.execute(sql)
 
-    def book_loan_info(self):
-        # 대출 정보 조회 기능 (심화)
-        # 대출한 도서의 정보를 모두 조회할 수 있습니다.
-        # 대출 정보는 도서의 ID, 이름, 저자, 출판사, 대출 날짜, 반납일자로 구성됩니다.
-        # 대출 정보는 대출 날짜를 기준으로 내림차순으로 정렬됩니다.
-        pass
+    result = db.cursor.fetchall()
+    print('+-------------------------')
+    print('|도서 정보 조회')
+    print('|-------------------------')
+    print('|도서ID | 도서명 | 저자 | 출판사 | 상태')
+    print('|-------------------------')
+    for book in result:
+        if book[4] == True:
+            print('|', book[0], '|', book[1], '|', book[2], '|', book[3], '|대출 가능')
+        else:
+            print('|', book[0], '|', book[1], '|', book[2], '|', book[3], '|대출 중')
+    print('|-------------------------')
+    input('|-> Enter : 메뉴로')
+    print('+-------------------------\n')
+    return
 
-    def book_input(self):
-        # 도서추가 데이터 입력 기능 (기본)
-        # (기본) 사용자는 콘솔을 통해 도서의 정보를 입력하여 데이터베이스에 저장합니다.
-        # (기본) 도서의 정보는 도서의 ID, 이름, 저자, 출판사 정보를 포함합니다.
-        # (심화) 파일을 통해 도서의 정보를 입력하여 데이터베이스에 저장합니다. (csv, json, xml 등)
-        pass
+def book_loan(searcher, db):
+    if searcher.isdigit():
+        update_sql = "UPDATE Books SET is_available = FALSE WHERE book_id= %s "
+        insert_sql = "INSERT INTO Loans VALUES (default, %s, now(), null);"
+
+        db.cursor.execute(insert_sql, searcher)
+        db.cursor.execute(update_sql, searcher)
+
+    else:
+        select_id = "SELECT book_id FROM Books WHERE title = %s " % ('\''+searcher+'\'')
+        db.cursor.execute(select_id)
+        select_id = db.cursor.fetchone()
+
+        update_sql = "UPDATE Books SET is_available = FALSE WHERE book_id= %s "
+        insert_sql = "INSERT INTO Loans VALUES (default, %s, now(), null);"
+
+        db.cursor.execute(insert_sql, select_id)
+        db.cursor.execute(update_sql, select_id)
+
+    print('+-------------------------')
+    print('|대출이 완료 되었습니다')
+    db.conn.commit()
+    print('|-------------------------')
+    input('|-> Enter : 메뉴로')
+    print('+-------------------------\n')
+    return
+
+
+def book_return(searcher, db):
+    # 도서 반납 기능 (기본)
+    # (기본) 반납을 원하는 도서의 ID 혹은 이름을 입력하여 반납합니다.
+    # (기본) 반납하면 도서의 상태가 대출 가능으로 변경됩니다.
+    pass
+    db.conn.commit()
+
+
+def book_loan_info():
+    # 대출 정보 조회 기능 (심화)
+    # 대출한 도서의 정보를 모두 조회할 수 있습니다.
+    # 대출 정보는 도서의 ID, 이름, 저자, 출판사, 대출 날짜, 반납일자로 구성됩니다.
+    # 대출 정보는 대출 날짜를 기준으로 내림차순으로 정렬됩니다.
+    pass
+
+def book_all(db):
+    sql = "SELECT * FROM Books;"
+    db.cursor.execute(sql)
+    result = db.cursor.fetchall()
+
+    print('+-------------------------')
+    print('|전체 도서 목록 조회')
+    print('|-------------------------')
+    print('|도서ID | 도서명 | 저자 | 출판사 | 상태')
+    print('|-------------------------')
+    for book in result:
+        if book[4] == True:
+            print('|', book[0], '|', book[1], '|', book[2], '|', book[3], '|대출 가능')
+        else:
+            print('|', book[0], '|', book[1], '|', book[2], '|', book[3], '|대출 중')
+    print('|-------------------------')
+    input('|-> Enter : 메뉴로')
+    print('+-------------------------\n')
+    return
--- main.py
+++ main.py
@@ -1,33 +1,42 @@
 # 도서 관리 콘솔 어플리케이션
-from book import Book
+import psycopg2
+
+from book import *
+from database import database
 from show import *
 
-book1 = Book('1', '제목', '이진호', '사자출판사', '대출가능')
+db = database(None, None)  # db 객체 생성
+db.open_db()  # db 열기
 
 while 1:
-    show_main()
-    main_choice = int(input('|-> 사용자 선택 : '))
-    print('+-------------------------')
+    main_choice = show_main()
 
     if main_choice == 1:  # 도서 정보 조회
-        book1.book_info()
+        searcher = show_sub('정보 조회')
+        book_info(searcher, db)
 
     elif main_choice == 2:  # 도서 대출
-        show_sub('대출')
-        book1.book_loan()
+        searcher = show_sub('대출')
+        book_loan(searcher, db)
 
     elif main_choice == 3:  # 도서 반납
-        show_sub('반납')
-        book1.book_return()
+        searcher = show_sub('반납')
+        book_return(searcher, db)
 
     elif main_choice == 4:  # 대출 정보 조회
-        book1.book_loan_info()
+        book_loan_info()
 
     elif main_choice == 5:  # 도서 추가
-        book1.book_input()
+        add = show_add()  # list 타입 리턴 도서 정보
+        newbook = Book(add[0], add[1], add[2], add[3])
+        newbook.book_input(db)
+
+    elif main_choice == 6: #전체 도서 조회
+        book_all(db)
 
     elif main_choice == 0:  # 종료
+        db.close_db()  # db 닫기
         break
 
     else:
-        print('다시 입력')
+        print('다시 입력 해주세요')
