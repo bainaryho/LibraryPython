5180c7e87273b3b9d81654583d6ea7cbb754a953
bainaryho <rkswl233@naver.com>
2023-07-10 16:58:13 +0900

    🔐 DB 보안 .env 추가 및 제출🔑

--- database.py
+++ database.py
@@ -1,18 +1,22 @@
 import psycopg2
-
+from dotenv import load_dotenv
+import  os
 
 class database:
     def __init__(self, conn, cursor):
         self.conn = conn
         self.cursor = cursor
 
+    load_dotenv()
+
     def open_db(self):
         self.conn = psycopg2.connect(
-            host="localhost",  # 호스트 값
-            port="5432",  # 포트 값 받아 오고
-            dbname="library",  # 다
-            user="user1",  # 받아
-            password="4444"  # 온다
+            #DB정보는 env파일에서 관리합니다.
+            host=os.environ.get("admin_host"),
+            port=os.environ.get("admin_port"),
+            dbname=os.environ.get("admin_dbname"),
+            user=os.environ.get("admin_user"),
+            password=os.environ.get("admin_password")
         )
         self.cursor = self.conn.cursor()
 
